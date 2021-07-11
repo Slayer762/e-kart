2566a943bf74725250412b2bfc99a6769e4d1a04
Slayer762 <demonk762@gmail.com>
2021-07-11 20:20:40 +0530

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -37,11 +37,11 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, '/frontend/build/index.html'))
-  );
-//} else {
-//  app.get('/', (req, res) => {
-//    res.send('Server is ready');
-//  })
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('Server is ready');
+  })
 };
 
 app.use((err, req, res, next) => {
