c219a9dd2d8ff73c22c4e716d31790c90eb4db27
Slayer762 <demonk762@gmail.com>
2021-07-19 11:22:13 +0530

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -134,3 +134,4 @@ httpServer.listen(port, () => {
 //"start": "nodemon --watch backend --exec node --experimental-modules backend/server.js",
 //"heroku-postbuild": "NPM_CONFIG_PRODUCTION=false cd frontend && npm install && npm run build"
 //MONGODB_URL=mongodb+srv://e-kart:e123k456@cluster0.okrxu.mongodb.net/e-kart?retryWrites=true&w=majority
+//MONGODB_URL=mongodb://localhost/e-kart
